efa34445a2e23397cf7e08161a57bfa67faf70eb
Matthieu Clin <matthieu.clin@outlook.com>
2016-11-11 16:39:27 +0100

    Indexation Factory
    Given a list of documents, indexes them all and add them to the reverse index
    The mapping between the document and the group of objects (readers, writers..)
     handling it  are made through the matching of the extension to a dictionary.

--- IndexationFactory.py
+++ IndexationFactory.py
@@ -5,35 +5,97 @@
 	vincent.jeanselme@gmail.com
 """
 
-from DocType.TextType import textType
-from IndexManip.TextIndexManip import textIndexManip
-from ReverseIndexManip.TextReverseIndexManip import textReverseIndexManip
+import os
 
-class indexationFactory:
+# Modules for text documents
+import DocType.TextType as TextType
+import IndexManip.TextIndexManip as TextIndexManip
+import ReverseIndexManip.TextReverseIndexManip as TextReverseIndexManip
+
+"""
+	key: file extension
+	value: indexation key that can handle the corresponding file extension
+"""
+matching_key = {
+	'.txt': 'text',
+	'.md': 'text'
+}
+
+keys = ['text', 'unknown']
+
+factory = {
+	'text': {
+		'doctype': TextType.textType,
+		'reader': TextIndexManip.textIndexReader,
+		'writer': TextIndexManip.textIndexWriter,
+		'reverse_writer': TextReverseIndexManip.textReverseIndexWriter,
+		'reverse_file_path': 'text.reverse.index',
+	}
+}
+
+
+def group_by_key(documents):
 	"""
-	Class which associates the fileNames with the associated docType and
-	IndexManip before computing reverseIndexes
+		Groups documents by their indexation key (see get_matching_key)
+	:param documents: List of documents
+	:return: Dict where keys are from IndexationFactory.keys
+					values are array with all document that matched
+	"""
+	grouped = {key: [] for key in keys}
+
+	for document in documents:
+		grouped[get_matching_key(document)].append(document)
+
+	return grouped
+
+
+def get_matching_key(document_path):
+	"""
+		Associate a document to the key of an indexation class that can handle it
+	:param 	document_path: a document path to test
+	:return: 	If the document can be handled, the corresponding indexation key
+				Otherwise 'unknown'
+	"""
+	s = os.path.splitext(document_path)
+	ext = s[1]
+
+	key = matching_key[ext] if ext in matching_key else 'unknown'
+
+	return key
+
+
+def create_index_reader(key, document):
+	"""
+		Indexes the document and open a reader on the index file
+	:param key:	should be the result of get_matching_key(document)
+	:param document: Document to index
+	:return: the corresponding reader object already initialized
 	"""
 
-	def __init__(self, fileNames, force = False):
-		"""
-		Inits the indexation with the fileNames
-		If force is true, the reindexation is computed
-		"""
-		# Init all the different index
-		tims = []
+	fac = factory[key]
+	text = fac['doctype'](document)
+	tiw = fac['writer'](text)
 
-		# Associates the good type of dataStructure and indexes before computing
-		# the corresponding reverseIndexes
-		for fileName in fileNames:
-			dot = fileName.rfind('.')
-			if fileName[dot:] in {".txt", ".md"}:
-				tim = textIndexManip()
-				text = textType(fileName)
-				tim.create(text)
-				tim.save()
-				tims.append(tim)
-			else:
-				print("Error : Unknown format of " + fileName)
+	return fac['reader'](tiw.savePath)
 
-		textReverseIndexManip("ReverseIndexes/text.reverse.index").create(tims)
+
+def create_reverse_indexes(documents):
+	"""
+		Indexes all documents according to their type and add them to their corresponding reverse index.
+		All texts documents will be indexes through text indexation and added to the text reverse index
+		Same for all future kind of document
+	:param documents: List of documents. Should all exist !
+	"""
+	grouped = group_by_key(documents)
+	for unknown in grouped['unknown']:
+		print("No associated objects. Cannot index file: {}".format(unknown))
+
+	del grouped['unknown']
+
+	for key, docs in grouped.items():
+		readers = []
+		for doc in docs:
+			readers.append(create_index_reader(key, doc))
+		fac = factory[key]
+
+		fac['reverse_writer'](fac['reverse_file_path'], readers)
---
+++ Tests/Unit/TestIndexationFactory.py
@@ -0,0 +1,63 @@
+"""
+    Search Engine
+    by Vincent Jeanselme
+    and Matthieu Clin
+    vincent.jeanselme@gmail.com
+"""
+
+import sys
+import os
+import unittest
+# Needs to be execute from the project source
+sys.path.append(os.getcwd())
+
+import IndexationFactory
+
+
+class TestIndexationEngine(unittest.TestCase):
+
+    def test_txt_file(self):
+        m = IndexationFactory.get_matching_key('myTextFile.txt')
+        self.assertEqual(m, 'text')
+
+    def test_md_file(self):
+        m = IndexationFactory.get_matching_key('myTextFile.md')
+        self.assertEqual(m, 'text')
+
+    def test_unknown_extension_file(self):
+        m = IndexationFactory.get_matching_key('myTextFile.unknown')
+        self.assertEqual(m, 'unknown')
+
+        m = IndexationFactory.get_matching_key('myTextFile.foobar')
+        self.assertEqual(m, 'unknown')
+
+    def test_no_extension_file(self):
+        m = IndexationFactory.get_matching_key('myTextFile')
+        self.assertEqual(m, 'unknown')
+
+    def test_group_texts(self):
+        docs = ['file1.txt', 'file2.txt']
+        grouped = IndexationFactory.group_by_key(docs)
+        expected = {'text': docs, 'unknown': []}
+        self.assertDictEqual(grouped, expected)
+
+    def test_group_texts_and_mds(self):
+        docs = ['file1.txt', 'file2.md']
+        grouped = IndexationFactory.group_by_key(docs)
+        expected = {'text': docs, 'unknown': []}
+        self.assertDictEqual(grouped, expected)
+
+    def test_group_texts_and_unknowns(self):
+        docs = ['file1.txt', 'file2.fake_extension']
+        grouped = IndexationFactory.group_by_key(docs)
+        expected = {'text': [docs[0]], 'unknown': [docs[1]]}
+        self.assertDictEqual(grouped, expected)
+
+    def test_create_index(self):
+        reader = IndexationFactory.create_index_reader('text', './Tests/Sources/Texts/test1.txt')
+        reader.ptr.close()
+        assert (os.path.exists('Indexes/test1.txt.text.index'))
+        os.remove('Indexes/test1.txt.text.index')
+
+if __name__ == '__main__':
+    unittest.main()
